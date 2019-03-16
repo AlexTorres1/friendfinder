1d3095100e80d15efdf2dac707c3d10541f74ab9
torn75 <torn_75@yahoo.com>
2019-03-16 19:16:15 -0400

    added freinds list

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friendsList = [
+    {
+    "name":"Caitlynn",
+    "photo":"https://images.unsplash.com/photo-1531251445707-1f000e1e87d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=562&q=80",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+        ]
+    },{
+    "name":"Daniela",
+    "photo":"https://unsplhttps://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80ash.com/photos/O3ymvT7Wf9U",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+        ]
+}];
+
+module.exports = friendsList;
